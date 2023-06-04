3613c7b381d55bf6f964d4f7dce542e574cffcc2
Michael Halpin <halpinm2010@gmail.com>
2023-06-04 19:31:24 +0100

    fixed?

--- postinstall.py
+++ postinstall.py
@@ -9,3 +9,18 @@ except PermissionError:
 os.system("pacman -S --noconfirm xorg xorg-server")
 os.system("pacman -S --noconfirm gnome")
 os.system("systemctl enable gdm.service")
+
+os.system("pacman -S --noconfirm xdg-utils")
+os.system(
+    "curl https://raw.githubusercontent.com/Orange-OS-LE/OrangeOSLE2/main/apps/ocular/ocular.png > ocular.png"
+)
+os.system("cp ocular.png /opt/ocular.png")
+os.system("rm ocular.png")
+
+os.system(
+    "curl https://raw.githubusercontent.com/Orange-OS-LE/OrangeOSLE2/main/apps/ocular/Ocular.desktop > Ocular.desktop"
+)
+os.system("cp Ocular.desktop /usr/share/applications/")
+os.system("rm Ocular.desktop")
+
+print('You can now reboot by running "sudo reboot -h now"')
