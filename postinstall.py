428daca4381d705c26223fb79aa2e2120c8d5381
jdev08 <92550746+jdev082@users.noreply.github.com>
2023-07-30 15:46:07 -0500

    feat: you can install multiple packages with one command

--- postinstall.py
+++ postinstall.py
@@ -6,17 +6,8 @@ except PermissionError:
     print('You must run this script by running "sudo python3 postinstall.py"')
     quit(0)
 
-os.system("pacman -S --noconfirm xorg xorg-server")
-os.system("pacman -S --noconfirm gnome")
+os.system("pacman -S --noconfirm xdg-utils vim firefox git xorg xorg-server gnome gdm")
 os.system("systemctl enable gdm.service")
-
-os.system("pacman -S --noconfirm xdg-utils")
-
-os.system("pacman -S vim --noconfirm")
-
-os.system("pacman -S firefox --noconfirm")
-os.system("pacman -S git")
-
 
 def install_apps():
     os.system("git clone https://github.com/OrangeOSLE2/PKGBUILDs"
