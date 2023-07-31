1d7f778df064de0e583b1d5d36a1bd55eb5ef90a
michaeleldar <66514999+michaeleldar@users.noreply.github.com>
2023-07-31 14:23:46 +0100

    fix noconfirm

--- postinstall.py
+++ postinstall.py
@@ -9,13 +9,13 @@ except PermissionError:
 with open('postinstall.pkgs') as pkgs:
     while pkgs.readable():
         for line in pkgs:
-            os.system(f'pacman -S {line.strip()} --no-confirm')
+            os.system(f'pacman -S {line.strip()} --noconfirm')
 
 os.system("sudo systemctl enable gdm.service")
 
 def install_apps():
     os.system("git clone https://github.com/OrangeOSLE2/PKGBUILDs")
-    os.system("pacman -U PKGBUILDs/packages/oosle-shortcuts-1.0-1-any.pkg.tar.zst --no-confirm")
-    os.system("pacman -U PKGBUILDs/packages/turbowarp-desktop-bin-1.8.1-1-any.pkg.tar.zst --no-confirm")
+    os.system("pacman -U PKGBUILDs/packages/oosle-shortcuts-1.0-1-any.pkg.tar.zst --noconfirm")
+    os.system("pacman -U PKGBUILDs/packages/turbowarp-desktop-bin-1.8.1-1-any.pkg.tar.zst --noconfirm")
 
 print('You can now reboot by running "sudo reboot -h now"')
