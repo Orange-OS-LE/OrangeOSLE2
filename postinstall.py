fa6cf3aa486dd95c352a4bc47216c0328fd78c08
michaeleldar <66514999+michaeleldar@users.noreply.github.com>
2023-07-31 13:55:29 +0100

    Add no-confirms

--- postinstall.py
+++ postinstall.py
@@ -9,7 +9,7 @@ except PermissionError:
 with open('postinstall.pkgs') as pkgs:
     while pkgs.readable():
         for line in pkgs:
-            os.system(f'pacman -S {line.strip()}')
+            os.system(f'pacman -S {line.strip()} --no-confirm')
 
 with open('postinstall.srvcs') as srvcs:
     while srvcs.readable():
@@ -18,7 +18,7 @@ with open('postinstall.srvcs') as srvcs:
 
 def install_apps():
     os.system("git clone https://github.com/OrangeOSLE2/PKGBUILDs")
-    os.system("pacman -U PKGBUILDs/packages/oosle-shortcuts-1.0-1-any.pkg.tar.zst")
-    os.system("pacman -U PKGBUILDs/packages/turbowarp-desktop-bin-1.8.1-1-any.pkg.tar.zst")
+    os.system("pacman -U PKGBUILDs/packages/oosle-shortcuts-1.0-1-any.pkg.tar.zst --no-confirm")
+    os.system("pacman -U PKGBUILDs/packages/turbowarp-desktop-bin-1.8.1-1-any.pkg.tar.zst --no-confirm")
 
 print('You can now reboot by running "sudo reboot -h now"')
